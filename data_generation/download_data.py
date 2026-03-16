4e94ff0c20c7f97c5eaf3ff7ad5d90b30523f8f5
Neskech <neskechastro@gmail.com>
2026-03-16 19:20:30 -0400

    Finished data downloading script

--- data_generation/download_data.py
+++ data_generation/download_data.py
@@ -8,10 +8,6 @@ FILE_ID = ''
 DATA_PATH = '../data/'
 
 
-def _download_zip_to_path(path: str):
-    download_from_google()
-
-
 def main():
     if os.path.exists(DATA_PATH):
         os.rmdir(DATA_PATH)
@@ -20,7 +16,13 @@ def main():
     os.makedirs(poses_path, exist_ok=False)
 
     zip_path = os.path.join(DATA_PATH, 'data.zip')
-    _download_zip_to_path(zip_path)
+    download_from_google(FILE_ID, 'data.zip', DATA_PATH)
     with zipfile.ZipFile(zip_path, 'r') as f:
         f.extractall(poses_path)
     os.remove(zip_path)
+
+
+if __name__ == "__main__":
+    print("Downloading data from google drive...")
+    main()
+    print("Finished downloading data from google drive!")
